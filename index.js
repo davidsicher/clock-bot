4dc1e01bdd972b4a6efb0b9bd0058a6a8addc697
David Sicher <dsicher@hugeinc.com>
2016-01-13 12:00:12 -0800

    fix timer bug

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ var sayRollCall = function(bot, message) {
 controller.hears(['roll call','role call'], publicMsg, sayRollCall);
 
 var sayCurrentTime = function(bot, message) {
-  bot.reply(message, 'ding it is ' + (((new Date().getUTCHours()+3)%13)+1) + ' oclock');
+  bot.reply(message, 'ding it is ' + (((new Date().getUTCHours()+4)%13)+1) + ' oclock');
 }
 controller.hears(['what time is it'], publicMsg, sayCurrentTime);
 
 var happyHours = ['Anchorage', 'Los Angeles', 'Phoenix', 'Winnipeg', 'Havana', 'Halifax', 'Buenos Aires', 'Sao Paulo', 'Rio de Janeiro', 'Reykjavik', 'Algiers', 'Cairo', 'Minsk', 'Dubai', 'Islamabad', 'Dhaka', 'Bangkok', 'Beijing', 'Tokyo', 'Brisbane', 'Melbourne', 'Anadyr', 'Auckland', 'Kiritimati']
 var sayCurrentHappyHour = function(bot, message) {
-  bot.reply(message, 'its always happy hour somewhere! right now its happy hour in: ' + happyHours[new Date().getUTCHours()]);
+  bot.reply(message, 'its always happy hour somewhere! right now its happy hour in: ' + happyHours[(new Date().getUTCHours()+4)%24]);
 }
 controller.hears(['drink', 'thirsty', 'happy'], publicMsg, sayCurrentHappyHour);
 
